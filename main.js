2b9cb9ef546995bd57c61775cfd7081a672cdf20
Silver <rhyskoerber@gmail.com>
2024-06-15 13:48:50 +0930

    e

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function updateConsole(scroll=true) {
     let h = canvas.height-200*su-msgBox.height - 20*su
     cmdC.set(x, y, w, h)
     let lines2 = ui.measureText(30*su, lines.join(" \n"), {wrap: w}).lines
-    cmdC.bounds.minY = -lines2*30*su*ui.fontSizeMul*ui.spacingMul+cmdC.height-30*su
+    cmdC.bounds.minY = -lines2*30*su*ui.fontSizeMul*ui.spacingMul+cmdC.height
     if (scroll) {
         cmdC.off.y = cmdC.bounds.minY
     }
@@ -35,15 +35,17 @@ function mainTick() {
 
     // cmdC.set(x, y, w, h)
     ui.setC(cmdC)
+    updateConsole(false)
     // cmdC.bounds.minY = -lines2*30*su*ui.fontSizeMul*ui.spacingMul+cmdC.height
     // cmdC.scrollY = cmdC.bounds.minY
     // cmdC.update()
 
-    ui.text(0, 15*su, 30*su, lines.join(" \n"), {wrap: w-20*su}).lines
+    ui.text(0, 15*su, 30*su, lines.join(" \n"), {wrap: w-20*su})
 
     ui.setC()
 
     msgBox.set(canvas.width/2, canvas.height-100*su-40*su/2, canvas.width-200*su, 40*su)
+    msgBox.outlineSize = 10*su
     msgBox.hover()
     msgBox.draw()
 
